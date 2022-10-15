88db94168d8183fe87eadf0488a97c70b2e52722
Olatoye Ibrahim Eniola <49520816+c-Teck@users.noreply.github.com>
2022-10-15 06:54:33 -0700

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-char i;
-char j;
+	char i;
+	char j;
 
-for (i = a; i <= z; i++)
-{
-putchar(i);
-}
-for (j = A; j <= Z; j++)
-{
-putchar(j);
-}
-putchar(n);
-return (0);
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (j = 'A'; j <= 'Z'; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
 }
