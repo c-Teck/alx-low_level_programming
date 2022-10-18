35004519d8978e1b8211a7435fbf6be94021b01c
Olatoye Ibrahim Eniola <49520816+c-Teck@users.noreply.github.com>
2022-10-18 12:12:40 -0700

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,42 +1,23 @@
 #include <stdio.h>
-
 /**
  * main - main block
- * Description: Print the first 50 fibonacci numbers, starting with 1 and 2.
- * Numbers must be coma and space separated.
+ * Description: computes and prints even  number < 4,000,000
+ * 5 below 1024 (excluded), followed by a new line
  * Return: 0
  */
-
 int main(void)
 {
-    int count = 3; /* this is so because the first 2 members have been given already. My loop will therefore begin from the 3rd one */
+	int a = 0, b = 1, next = 0;
+	int sum = 0;
 
-    long int first = 1, second = 2;
-    long int next = first + second;
-
-    printf("%lu, ", first);
-    printf("%lu, ", second);
-
-    while (count <= 50)
-    {
-        /* Let's check if we are at the end of the list, if we are close with a new line */
-        if (count == 50)
-        {
-            printf("%lu \n", next);
-         }
-         else  /* if we are not at the end of the list, add a comma after the number */
-         { 
-           printf("%lu, ", next); 
-         }
-
-        /* Reset the variables to get the next number */
-        first = second;
-        second = next;
-
-        /* after resetting the variables, you need to find the next number */
-        next = first + second; 
-        count++;
-    }
-
-    return (0);
+	while (next < 4000000)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+		if (next % 2 == 0)
+			sum += next;
+	}
+	printf("%i\n", sum);
+	return (0);
 }
