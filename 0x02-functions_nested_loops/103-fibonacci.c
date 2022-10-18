403faf6939ab8d5759337e3536c1719e0002a6f7
Olatoye Ibrahim Eniola <49520816+c-Teck@users.noreply.github.com>
2022-10-18 11:47:54 -0700

    update fibo

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - fibonacci
+ * main - main block
  * Description: Print the first 50 fibonacci numbers, starting with 1 and 2.
  * Numbers must be coma and space separated.
- * Return: 0(Sucess)
+ * Return: 0
  */
 
 int main(void)
 {
     int count = 3; /* this is so because the first 2 members have been given already. My loop will therefore begin from the 3rd one */
 
-    int first = 1, second = 2;
-    int next = first + second;
+    long int first = 1, second = 2;
+    long int next = first + second;
+
+    printf("%lu, ", first);
+    printf("%lu, ", second);
 
     while (count <= 50)
     {
