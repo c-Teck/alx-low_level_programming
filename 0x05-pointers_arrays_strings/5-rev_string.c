2dbd9e881f04c614897c6b310871e740160e04b3
Olatoye Ibrahim Eniola <49520816+c-Teck@users.noreply.github.com>
2022-10-28 13:26:01 -0500

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,21 +1,46 @@
 #include "main.h"
 /**
- * print_rev - reverse something
- * @s: params
- */
-
-void print_rev(char *s)
+*rev_string - Reverse a string
+*@s: String to reverse
+*Return: Nothing
+*/
+void rev_string(char *s)
 {
-	int i = 0;
-	int j;
+int i = 0, length;
+length = _strlen(s) - 1;
+while (length > i)
+{
+swap_char(s + length, s + i);
+i++;
+length--;
+}
+}
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	for (j = i - 1; j >= 0; j--)
-	{
-		_putchar(s[j]);
-	}
-	_putchar('\n');
+/**
+ *_strlen - returns the length of a string
+ * @s: string
+ *Return: returns lenght;
+ */
+int _strlen(char *s)
+{
+int count, inc;
+inc = 0;
+for (count = 0; s[count] != '\0'; count++)
+inc++;
+
+return (inc);
+}
+
+/**
+*swap_char - swap two characters
+*@a: first character
+*@b: second character
+*Return: nothing
+*/
+
+void swap_char(char *a, char *b)
+{
+char tmp = *a;
+*a = *b;
+*b = tmp;
 }
