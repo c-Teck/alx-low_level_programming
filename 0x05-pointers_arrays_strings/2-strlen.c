e422ac7772472e264adc514086abf433de95bf18
Olatoye Ibrahim Eniola <49520816+c-Teck@users.noreply.github.com>
2022-10-27 20:12:21 -0500

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,13 +4,14 @@
  * Description: this will return the length of a string
  * Return: 0 is success
  */
-int _strlen(char *s)
-{
+int _strlen(char *s) {
+	
 	int a = 0;
 
-	for (; *s++;){
+	for (; *s++;) {
 		a++;
 	}
+	
 	return (a);
 	
 }
